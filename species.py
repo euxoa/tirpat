10ec78479fc968a131d28f5c0b43fbc551acfff8
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2022-12-25 21:46:31 +0200

    species filter, args for clipping (not implemented)

--- species.py
+++ species.py
@@ -12,8 +12,11 @@ parser.add_argument("-n", "--nrows", type=int, default=3,
                     help="max number of rows per species (default: 3)")
 parser.add_argument('--full', action=argparse.BooleanOptionalAction,
                     help="require full number of lines for species")
+parser.add_argument('--species',
+                    help="species regex")
 parser.add_argument("input_files", nargs="+",
                     help="input files")
+parser.add_argument('--clip', nargs=2, help='clip input and output directories')
 
 args = parser.parse_args()
 
@@ -21,7 +24,12 @@ pmin = args.pmin
 minlag = args.minlag
 nrows = args.nrows
 req_full = args.full
+sp_rex = args.species
 input_files = args.input_files
+
+if args.clip is not None:
+    raw_dir = args.clip[0] 
+    clip_dir = args.clip[1]
 
 # pd.set_option('display.max_rows', None)
 
@@ -39,16 +47,30 @@ d.rename(columns = {"Scientific name" : "species",
                     "Common name" : "cname",
                     "Start (s)" :"start", "End (s)" : "end", "Confidence" : "p"}, inplace=True)
 
+# FIXME: filter by species
+if sp_rex is not None:
+    d = d.loc[d.species.str.contains(sp_rex) | d.cname.str.contains(sp_rex) ]
+
 d['t_within'] = (d['start'] + d['end'])/2
 d['t'] = (pd.to_datetime(d.file, format="%Y%m%d_%H%M%S", exact=False, utc=True) +
           pd.to_timedelta(d['t_within'], unit='s'))
 d['nicetime'] = d.t.dt.tz_convert('EET').dt.strftime('%A %d.%m. %H:%M')
     
 
+# .droplevel(0) or .droplevel('species') is nice but crashes
+# if you have zero species (empty data frame) after species rex for example.
+# the group_keys thing is needed for the same reason.
+# So, first take only trustworthy lines, and remove adjacent obs.
 d2 = d.pipe(lambda df: df[df['p'] > pmin]).\
-    groupby('species').apply(lambda x: deduplicate(x, 't', pd.to_timedelta(minlag, unit='s'))).droplevel((0)).\
-    groupby('species').apply(lambda x: x.sort_values('p', ascending=False).head(nrows)).droplevel(0).\
-    loc[:,('species' ,'cname', 'p', 'nicetime')]
+    groupby('species', group_keys=True).\
+    apply(lambda x: deduplicate(x, 't', pd.to_timedelta(minlag, unit='s'))).\
+    reset_index(drop=True)
+# Head, or only the best rows.
+d2 = d2.groupby('species', group_keys=True).\
+    apply(lambda x: x.sort_values('p', ascending=False).head(nrows)).\
+    reset_index(drop=True)
+# For output only (FIXME, this should be later).
+d2 = d2.loc[:,('species' ,'cname', 'p', 'nicetime')]
 
 if req_full:
     d2 = d2.groupby('species').filter(lambda x: x.shape[0] == nrows)
