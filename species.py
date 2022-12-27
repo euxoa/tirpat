29563d945389fc299cf17f641c7668710f1f001a
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2022-12-27 22:35:01 +0200

    Better close duplicate removal, clipping refinements etc.

--- species.py
+++ species.py
@@ -44,6 +44,8 @@ parser.add_argument("-n", "--nrows", type=int, default=3,
                     help="max number of rows per species (default: 3)")
 parser.add_argument('--full-only', action=argparse.BooleanOptionalAction,
                     help="require max number (nrows) of lines for species")
+parser.add_argument('--nonfull-only', action=argparse.BooleanOptionalAction,
+                    help="require less than max number (nrows) of lines for species")
 parser.add_argument('--species',
                     help="species regex to filter with")
 parser.add_argument("input_files", nargs="+",
@@ -79,11 +81,21 @@ else:
 # pd.set_option('display.max_rows', None)
 
 def deduplicate(d, var, threshold):
+    """
+    Find rows in d that are closer to each other than threshold by values of var,
+    then select only first row within each such adjacency group.
+    """
     d = d.sort_values(var)
-    # FIXME: This should actually calculate a cumsum, group by that,
-    # and select max or some such by another column.
-    # Note: the negation needed, otherwise first NaN goes wrong.
     return d[~d[var].diff(periods=1).abs().lt(threshold).values]
+
+def deduplicate2(d, var, threshold, var_max):
+    """
+    Find rows in d that are closer to each other than threshold by values of var,
+    then select the row highest in value of var_max in each 'adjacency group'.
+    """
+    return d.sort_values(var).\
+        groupby((~d[var].diff(periods=1).abs().lt(threshold).values).cumsum()).\
+        apply(lambda x: x.loc[x[var_max].idxmax()])
 
 d = pd.concat([pd.read_csv(f, header=0).assign(file=f)
                for f in input_files]) # was glob.glob(res/res-*.txt")
@@ -92,65 +104,54 @@ d.rename(columns = {"Scientific name" : "species",
                     "Common name" : "cname",
                     "Start (s)" :"start", "End (s)" : "end", "Confidence" : "p"}, inplace=True)
 
-# FIXME: filter by species
+# Filter by confidencce, and maybe by species
+d = d.loc[d['p'] > pmin]
 if sp_rex is not None:
     d = d.loc[d.species.str.contains(sp_rex) | d.cname.str.contains(sp_rex) ]
 
 d['t_center'] = (d['start'] + d['end'])/2
 d['t'] = (pd.to_datetime(d.file, format="%Y%m%d_%H%M%S", exact=False, utc=True) +
           pd.to_timedelta(d['t_center'], unit='s'))
-d['nicetime'] = d.t.dt.tz_convert(timezone).dt.strftime('%A %d.%m. %H:%M') # For output
-d['utctime'] = d.t.dt.strftime('%Y-%m-%d %H:%M UTC') # For clip metadata
     
 
 # .droplevel(0) or .droplevel('species') is nice but crashes
-# if you have zero species (empty data frame) after species rex for example.
-# the group_keys thing is needed for the same reason.
+# if you have zero species (empty data frame after species rex for example).
+# The group_keys thing is needed for the same reason.
 # With grouping, another way around would be groupby(..., as_index=False), then
 # you don't need to reset the index afterwards.
 
-# So, first take only trustworthy lines, and remove adjacent obs.
-d_trust = d.pipe(lambda df: df[df['p'] > pmin]).\
-    groupby('species', group_keys=True).\
-    apply(lambda x: deduplicate(x, 't', pd.to_timedelta(minlag, unit='s'))).\
-    reset_index(drop=True)
+# So, first take only trustworthy lines, and remove adjacent obs (by selecting argmax(p) of those)
+d_spaced = d.groupby('species', group_keys=True).\
+          apply(lambda x: deduplicate2(x, 't', pd.to_timedelta(minlag, unit='s'), 'p')).\
+          reset_index(drop=True)
 
 # Counts (maybe not needed)
-d_counts = d_trust.groupby('species', as_index=False).size().rename(columns={'size':'count'})
-
+d_counts = d_spaced.groupby('species', as_index=False).size().rename(columns={'size':'count'})
 
 # Head, or only the best rows.
-d_samples = d_trust.groupby('species', group_keys=True).\
+d_samples = d_spaced.groupby('species', group_keys=True).\
     apply(lambda x: x.sort_values('p', ascending=False).head(nrows)).\
     reset_index(drop=True)
 
 if args.full_only:
     d_samples = d_samples.groupby('species').filter(lambda x: x.shape[0] == nrows)
+if args.nonfull_only:
+    d_samples = d_samples.groupby('species').filter(lambda x: x.shape[0] < nrows)
 
-
-# {'start': 2533.5, 'end': 2536.5, 'species': 'Strix aluco', 'cname': 'lehtopöllö',
-#  'p': 0.5075, 'file': 'res/res-home-20221225_064256.txt',
-#  't_center': 2535.0, 't': Timestamp('2022-12-25 07:25:11+0000', tz='UTC'),
-#  'nicetime': 'Sunday 25.12. 09:25'}
-
-# We need to read ls() from the raw dir, then do the re match thing below
-# for files there to get time -> filename mapping into a dir,
-# then use that to get input files from res filenames.
-# Note that it doesn't need to exist, that should return a note for
-# that soxline.
-
+d_samples['nicetime'] = d_samples.t.dt.tz_convert(timezone).dt.strftime('%A %d.%m. %H:%M') # For output
+d_samples['utctime'] = d_samples.t.dt.strftime('%Y-%m-%d %H:%M UTC') # For clip metadata
+d_samples['nicetime2'] = d_samples.t.dt.tz_convert(timezone).dt.strftime('%Y%m%d_%H%M') # For clip names
 
 if not do_clip:
     # Just show the obs list or counts
     if args.counts:
-        # Show counts with max p, for species on the list (note potential --full-only)
+        # Counts with max p, for species on the list (note potential --full-only)
         d_show = d_samples.groupby(['species', 'cname'], as_index=False)['p'].max().\
             merge(d_counts).sort_values('count', ascending=False)
-        print(d_show.to_string())
     else:
-        # Show best obs per species
+        # Just best observations per species
         d_show = d_samples.loc[:,('species' ,'cname', 'p', 'nicetime')]
-        print(d_show.to_string())
+    print(d_show.to_string(index=False))
 else:
     # Make clips
     date_ptrn = re.compile(r"(\d{8}_\d{6})")
@@ -164,13 +165,12 @@ else:
             tmax = float(subprocess.run(['soxi', '-D', orig],
                                                  stdout=subprocess.PIPE, text=True).stdout)
             start, p, species, utctime = r['start'], r['p'], r['species'], r['utctime']
+            t_center, nicetime2 = r['t_center'], r['nicetime2']
             # Comment goes to the file as metadata.
-            t_center = r['t_center']
-            date = file_ptrn #.split('_')[0]
             comment = (f"species={species}, confidence={p}, time={utctime}, "
                        f"orig_file={orig}, t_center={t_center}")
-            hsh = hashlib.md5(comment.encode('latin1')).hexdigest()[:4]
-            clip = f"{clip_dir}/{r['cname']}_{date}_{hsh}.{output_type}"
+            hsh = f"{int(100 * p)}{hashlib.md5(comment.encode('latin1')).hexdigest()[:2]}"
+            clip = f"{clip_dir}/{r['cname']}_{nicetime2}_{hsh}.{output_type}"
             th = output_duration / 2 
             t0, pd0 = (virt0, 0) if (virt0 := t_center - th) > 0 else (0, -virt0) 
             t1, pd1 = (virt1, 0) if (virt1 := t_center + th) < tmax else (tmax, virt1 - tmax)
@@ -178,11 +178,12 @@ else:
             sox1 = subprocess.Popen(['sox', orig, 
                                      '-t', 'flac', '-', # output (stdout)
                                      'trim', str(t0), str(t1-t0),
-                                     'highpass', str(50),
+                                     'highpass', str(100),
                                      'norm', str(-4)],
                                     stdout=subprocess.PIPE)
             sox2 = subprocess.run(['sox', '-', 
                                    '--comment', comment, # add metadata
+                                   '-b', '16',
                                    clip, # output file (final)
                                    'pad', str(pd0), str(pd1)],
                                   stdin=sox1.stdout)
